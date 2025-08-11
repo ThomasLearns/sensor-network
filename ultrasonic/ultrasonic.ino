04d7a720197a567aa970db5e8d89f86b080e01eb
William Black <william.s.black48.civ@us.navy.mil>
2025-08-11 09:59:23 -0700

    interval for packet sending set up

--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -1,8 +1,28 @@
 
-void setup() {
+// time between sending packets
+unsigned long sendIntervalMs = 10;
+// time of last packet sent
+unsigned long lastSendMs = 0;
 
+// send a packet to the coordinator
+void sendPacket() {
+  // mark now as the last time a packet was sent
+  lastSendMs = millis()
 }
 
-void loop() {
+// runs at startup
+void setup() {
+  // send the first packet
+  sendPacket()
+}
 
+// runs on repeat
+void loop() {
+  // used to access the time of this loop
+  unsigned long currentTimeMs = millis();
+
+  // if enough time has passed, send a packet to the coordinator
+  if (currentTimeMs - lastSendMs >= sendIntervalMs) {
+    sendPacket();
+  }
 }
