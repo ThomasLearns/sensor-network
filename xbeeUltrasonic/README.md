977ea42d46affdd6daf62b752b6651329f4ffabe
William Black <william.s.black48.civ@us.navy.mil>
2025-09-29 15:19:43 -0700

    created sketch for ultrasonic sensors using the XBee network

---
+++ xbeeUltrasonic/README.md
@@ -0,0 +1,5 @@
+# XBee Ultrasonic
+
+## Uploading
+
+You **MUST** ensure that the switch on the XBee shield is set to `DLINE` before uploading. During operation, the switch should be set to `UART`.
---
+++ xbeeUltrasonic/xbeeUltrasonic.ino
@@ -0,0 +1,30 @@
+#include <XbeeRadio.h>
+#include <UltrasonicSensor.h>
+
+// Collect data from an ultrasonic sensor and send it to the coordinator
+// with an XBee radio module.
+
+// sensor id
+#define routNumber 9
+
+// sensor wiring
+#define triggerPin 12
+#define echoPin 11
+
+// construct sensor
+UltrasonicSensor sensor(
+  triggerPin,
+  echoPin,
+  routNumber,
+  // direct sensor data to xbee radio
+  xbee::sendToCoordinator
+);
+
+void setup() {
+  xbee::setup();
+}
+
+void loop() {
+  xbee::loop();
+  sensor.loop();
+}
